b3f9d1f980c9c2a729032cb68ffd7f30c3d75190
Abdiel <abdieldeathayde67@gmail.com>
2021-08-09 20:47:34 -0300

    feat: update file views

--- venv_trabalho/trabalhoFinal/main/views.py
+++ venv_trabalho/trabalhoFinal/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Ingrediente
 
 # Create your views here.
+
+def list_page(request):
+    obj=Ingrediente.objects.all()
+    
+    return render (request, 'list.html', {'obj':obj})
