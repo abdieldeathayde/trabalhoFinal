dcfec48a9806efb61b5863fd7237a819a2865e29
Abdiel <abdieldeathayde67@gmail.com>
2021-08-16 21:38:30 -0300

    feat: updade file urls

--- venv_trabalho/trabalhoFinal/trabalhoFinal/urls.py
+++ venv_trabalho/trabalhoFinal/trabalhoFinal/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.EscolheRoupa, name='roupas'),
 ]
