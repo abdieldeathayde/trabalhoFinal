691fc3ac81b257ea7bd856281aa2606aed8ac8bd
Abdiel <abdieldeathayde67@gmail.com>
2021-08-16 21:32:56 -0300

    feat: update file admin

--- venv_trabalho/trabalhoFinal/main/admin.py
+++ venv_trabalho/trabalhoFinal/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingrediente
+from .models import Roupa
 
 # Register your models here.
-admin.site.register(Ingrediente)
+admin.site.register(Roupa)
